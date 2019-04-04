0da7ce79a0b553bdcf8a4c2e7b936b459e735439
JustHappyGuest <JustHappyGuest@gmail.com>
2019-04-04 14:07:44 +0300

    add task libs

--- gulpfile.js
+++ gulpfile.js
@@ -31,56 +31,67 @@ function onBuild(cb){
 
 gulp.task("clean", function(){
   return gulp.src("./dist/**/", {read : false})
-          .pipe(clean());
+      .pipe(clean());
 });
 
 gulp.task("views", function(){
   return gulp.src("./app/*.html")
-          .pipe(gulpif(build, replace("style.css", "style.min.css")))
-          .pipe(gulpif(build, replace("slider.js", "main.min.js")))
-          .pipe(gulp.dest("./dist/"))
-          .pipe(browserSync.stream());
+      .pipe(gulpif(build, replace("style.css", "style.min.css")))
+      .pipe(gulpif(build, replace("main.js", "main.min.js")))
+      .pipe(gulpif(build, replace("libs.js", "libs.min.js")))
+      .pipe(gulp.dest("./dist/"))
+      .pipe(browserSync.stream());
 })
 
 gulp.task("styles", function(){
   return gulp.src("./app/scss/**/*.scss")
-          .pipe(sass().on("error", sass.logError))
-          .pipe(autoprefixer(["last 12 versions", "> 1%", "ie 8", "ie 7"]))
-          .pipe(gulpif(build, cleanCSS()))
-          .pipe(gulpif(build, rename({suffix : ".min"})))
-          .pipe(gulp.dest("./dist/css"))
-          .pipe(browserSync.stream());
+      .pipe(sass().on("error", sass.logError))
+      .pipe(autoprefixer(["last 12 versions", "> 1%", "ie 8", "ie 7"]))
+      .pipe(gulpif(build, cleanCSS()))
+      .pipe(gulpif(build, rename({suffix : ".min"})))
+      .pipe(gulp.dest("./dist/css"))
+      .pipe(browserSync.stream());
 });
 
 gulp.task("scripts", function(){
-  return gulp.src("./app/js/**/*.js")
-          .pipe(concat("main.js"))
-          .pipe(gulpif(build, uglify()))
-          .pipe(gulpif(build, rename({suffix : ".min"})))
-          .pipe(gulp.dest("./dist/js/"))
-          .pipe(browserSync.stream());
+  return gulp.src(["./app/js/**/*.js"])
+      .pipe(concat("main.js"))
+      .pipe(gulpif(build, uglify()))
+      .pipe(gulpif(build, rename({suffix : ".min"})))
+      .pipe(gulp.dest("./dist/js/"))
+      .pipe(browserSync.stream());
+});
+
+gulp.task("libs", function(){
+  return gulp.src(["./app/libs/**/*.js"])
+      .pipe(concat("libs.js"))
+      .pipe(gulpif(build, uglify()))
+      .pipe(gulpif(build, rename({suffix : ".min"})))
+      .pipe(gulp.dest("./dist/libs/"))
+      .pipe(browserSync.stream());
 });
 
 gulp.task("images", function(){
   return gulp.src("./app/img/**/*.{jpg,jpeg,png,gif,svg}")
-          .pipe(gulpif(build, imagemin()))
-          .pipe(gulp.dest("./dist/img/"));
+      .pipe(gulpif(build, imagemin()))
+      .pipe(gulp.dest("./dist/img/"));
 });
 
 gulp.task("fonts", function(){
   return gulp.src("./app/fonts/**/*.{otf,ttf,woff,woff2}")
-          .pipe(gulp.dest("./dist/fonts/"));
+      .pipe(gulp.dest("./dist/fonts/"));
 });
 
 gulp.task("watches", function(){
   gulp.watch("app/scss/**/*.scss", gulp.series("styles"));
   gulp.watch("app/**/*.html", gulp.series("views"));
   gulp.watch("app/js/**/*.js", gulp.series("scripts"));
+  gulp.watch("app/libs/**/*.js", gulp.series("libs"));
   gulp.watch("app/img/**/*.{jpg,jpeg,png,gif,svg}", gulp.series("images"));
   gulp.watch("app/fonts/**/*.{otf,ttf,woff,woff2}", gulp.series("fonts"));
 });
 
 
-exports.default = gulp.series("clean", gulp.parallel("views", "styles", "scripts", "images", "fonts"), gulp.parallel("watches", "browserSync"))
+exports.default = gulp.series("clean", gulp.parallel("views", "styles", "scripts", "libs", "images", "fonts"), gulp.parallel("watches", "browserSync"))
 
-exports.build = gulp.series(onBuild, "clean", "views", "styles", "scripts", "images", "fonts");
+exports.build = gulp.series(onBuild, "clean", "views", "styles", "scripts", "libs", "images", "fonts");
